b61dffbc76edfc4650dab96b2651a4ab0c5c0707
bilal iken <bilal11iken@gmail.com>
2025-12-28 01:51:42 +0100

    variable

--- Learning.java
+++ Learning.java
@@ -1,5 +1,26 @@
+// public class Learning {
+//   public static void main(String[] args) {
+//         if (args.length > 0) {
+//          System.out.println("Salam " + args[0] + "!");
+//         } else {
+//             System.out.println("Salam! ");
+//         }
+//     }
+// }
+
 public class Learning {
-    public static void main (String[] args){
-        System.out.println("hello word");
+    public static void main(String[] args){
+        int age = 12;
+        String name = "Ahmed";
+        char grade = 'A';
+        boolean minor = true;
+        double taile = 1.77;
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(minor);
+        System.out.println(taile);
+
     }
 }
