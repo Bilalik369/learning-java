dd9adbb6db08ff0abbf18a7804c72a4c09a8c43b
bilal iken <bilal11iken@gmail.com>
2025-12-28 02:00:07 +0100

    condition

--- Learning.java
+++ Learning.java
@@ -8,19 +8,36 @@
 //     }
 // }
 
-public class Learning {
-    public static void main(String[] args){
-        int age = 12;
-        String name = "Ahmed";
-        char grade = 'A';
-        boolean minor = true;
-        double taile = 1.77;
+// public class Learning {
 
-        System.out.println(age);
-        System.out.println(name);
-        System.out.println(grade);
-        System.out.println(minor);
-        System.out.println(taile);
+//     public static void main(String[] args){
+//         int age = 12;
+//         String name = "Ahmed";
+//         char grade = 'A';
+//         boolean minor = true;
+//         double taile = 1.77;
+
+//         System.out.println(age);
+//         System.out.println(name);
+//         System.out.println(grade);
+//         System.out.println(minor);
+//         System.out.println(taile);
+
+//     }
+// }
+
+
+public class Learning {
+  public static void main(String[] args) {
+
+    int age  = Integer.parseInt(args[0]);
+
+    System.out.println("entre votre age" + age);
+    if(age >= 18){
+        System.out.println("agee");
+    }else{
+        System.out.println("minore");
+    }
 
     }
 }
